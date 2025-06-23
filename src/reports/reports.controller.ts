b3411f8a81a344aab5a73958c728731df065697d
abidrafinurhastama <abidrafinurhastama@gmail.com>
2025-06-23 16:01:16 +0700

    [Feature] add get query api for estimate cars value

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,5 +1,15 @@
-import { Controller, Post, Body, UseGuards, Patch, Param } from '@nestjs/common'
+import {
+  Controller,
+  Post,
+  Body,
+  UseGuards,
+  Patch,
+  Param,
+  Get,
+  Query,
+} from '@nestjs/common'
 import { CreateReportDto } from './dtos/create-report.dto'
+import { GetEstimateDto } from './dtos/get-estimate.dto'
 import { ReportsService } from './reports.service'
 import { AuthGuard } from '../guards/auth.guard'
 import { CurrentUser } from '../users/decorators/current-user.decorator'
@@ -12,6 +22,11 @@ import { AdminGuard } from '../guards/admin.guard'
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    return this.reportsService.createEstimate(query)
+  }
 
   @Post()
   @UseGuards(AuthGuard)
